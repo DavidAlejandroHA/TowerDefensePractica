ea987dd7bf43dddfa944dbf71642d8b6f34180a9
David Alejandro <99730787+DavidAlejandroHA@users.noreply.github.com>
2024-01-18 15:12:43 +0000

    Añadido menú principal, créditos y opción de salir

---
+++ Assets/Scripts/MenuPrincipal/CambiarEscena.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscena : MonoBehaviour
+{
+    public string nombreEscena;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void cambiarEscena()
+    {
+        SceneManager.LoadScene(nombreEscena);
+    }
+
+    public void salirJuego()
+    {
+        Debug.Log("a");
+        Application.Quit();
+    }
+}
